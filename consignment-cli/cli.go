c6b59b76b157fb22658621320762817ba601b12e
Will <will7989@hotmail.com>
2019-04-01 12:15:47 +0100

    added the client into a dockerized env

---
+++ consignment-cli/cli.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	pb "GRPC-Demo/consignment-service/proto/consignment"
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	microclient "github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/cmd"
+)
+
+const (
+	address         = "localhost:50051"
+	defaultFilename = "consignment.json"
+)
+
+func parseFile(file string) (*pb.Consignment, error) {
+	var consignment *pb.Consignment
+
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	json.Unmarshal(data, &consignment)
+	return consignment, err
+}
+
+func main() {
+
+	cmd.Init()
+	client := pb.NewShippingServiceClient("go.micro.srv.consignment", microclient.DefaultClient)
+
+	file := defaultFilename
+	if len(os.Args) > 1 {
+		file = os.Args[1]
+	}
+
+	consignment, err := parseFile(file)
+
+	if err != nil {
+		log.Fatalf("could not parse file: %v", err)
+	}
+
+	r, err := client.CreateConsignment(context.Background(), consignment)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+
+	log.Printf("created: %t", r.Created)
+
+	getAll, err := client.GetConsignments(context.Background(), &pb.GetRequest{})
+
+	if err != nil {
+		log.Fatalf("could not list consignments: %v", err)
+	}
+
+	for _, v := range getAll.Consignments {
+		log.Println(v)
+	}
+
+}
