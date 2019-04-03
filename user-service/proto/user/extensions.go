4a7f273b3cd555334604d354d47c3e2bcdb0024a
Will <will7989@hotmail.com>
2019-04-03 14:55:17 +0100

    use different uuid lib

--- user-service/proto/user/extensions.go
+++ user-service/proto/user/extensions.go
@@ -1,13 +1,13 @@
 package go_micro_srv_user
 
 import (
+	"github.com/google/uuid"
 	"github.com/jinzhu/gorm"
-	"github.com/satori/go.uuid"
 )
 
 // BeforeCreate will run before a creation
 func (model *User) BeforeCreate(scope *gorm.Scope) error {
-	uuid, err := uuid.NewV4()
+	uuid, err := uuid.NewUUID()
 
 	if err != nil {
 		return err
