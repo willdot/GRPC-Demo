1d98517c0c77c8a8455f1a8584392253555e5fae
Will <will7989@hotmail.com>
2019-04-04 20:33:33 +0100

    added api

--- user-service/repository.go
+++ user-service/repository.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/jinzhu/gorm"
 	pb "github.com/willdot/GRPC-Demo/user-service/proto/auth"
 )
@@ -53,6 +55,7 @@ func (repo *UserRepository) GetByEmail(email string) (*pb.User, error) {
 
 // Create will create a new user
 func (repo *UserRepository) Create(user *pb.User) error {
+	log.Println(user)
 	if err := repo.db.Create(user).Error; err != nil {
 		return err
 	}
