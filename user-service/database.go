9f74a414e2002e795147580c1c3c4b17b4648cc6
Will <will7989@hotmail.com>
2019-04-08 14:04:45 +0100

    working on getting cassandra working with services

--- user-service/database.go
+++ user-service/database.go
@@ -12,7 +12,7 @@ var Session *gocql.Session
 func init() {
 	var err error
 	cluster := gocql.NewCluster("127.0.0.1")
-	cluster.Port = 9042
+	//cluster.Port = 9042
 	cluster.Keyspace = "shippy"
 	Session, err = cluster.CreateSession()
 	if err != nil {
